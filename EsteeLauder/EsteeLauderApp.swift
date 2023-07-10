26b8da019e54d907c8a4ec86e3b5bf8566809975
amagtoto <anmagtoto@gmail.com>
2023-07-09 22:10:12 -0700

    Initial Commit

---
+++ EsteeLauder/EsteeLauderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EsteeLauderApp.swift
+//  EsteeLauder
+//
+//  Created by Angela Magtoto on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct EsteeLauderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
